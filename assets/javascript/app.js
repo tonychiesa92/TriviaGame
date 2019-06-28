2ce3fc0cf3ee7f80d049f2fd99fb96b5d60f87ad
Tony C <tonychiesa92@gmail.com>
2019-06-28 17:15:09 -0400

    after clicking start button first question and answers will appear

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,55 @@
-var number = 100;
+// set variables
+var countDown = "";
+var currentQuestion = "";
+var currentChoices = "";
+var pointer = 0;
+var panel = $("#current-choices");
 
-//  Variable that will hold our interval ID when we execute
-//  the "run" function
-var intervalId;
+// $("#count-down").text(countDown);
+// $("#current-question").text(currentQuestion);
+// $("#current-choices").text(currentChoices);
 
-//  When the stop button gets clicked, run the stop function.
-$("#stop").on("click", stop);
 
-//  When the resume button gets clicked, execute the run function.
-$("#resume").on("click", run);
 
-//  The run function sets an interval
-//  that runs the decrement function once a second.
-//  *****BUG FIX******** 
-//  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
-function run() {
-  clearInterval(intervalId);
-  intervalId = setInterval(decrement, 1000);
+
+var questions = [{
+    question: "What is the only country that borders both the Caspian Sea and the Persian Gulf?",
+    answer: ["Iraq", "Iran", "Saudi Arabia", "Turkey"],
+    correctAnswer: "Iran"
+}, {
+    question: "The invasion of Fort Sumter was part of which war?",
+    answer: ["French & Indian War", "American Revolutionary War", "American Civil War", "Spanish-American War"],
+    correctAnswer: "American Civil War"
+}, {
+    question: "What city is the capital of North Dakota?",
+    answer: ["Fargo", "Grand Forks", "Pierre", "Bismark"],
+    correctAnswer: "Bismark"
+}];
+
+$("#start-button").on("click", function () {
+    $(this).hide();
+    game.displayQuestion(pointer);
+    game.displayChoices(pointer);
+});
+
+
+var game = {
+    wins: 0,
+    losses: 0,
+
+    displayQuestion: function (pointer) {
+        currentQuestion = questions[pointer].question;
+        $("#current-question").text(currentQuestion);
+    },
+    displayChoices: function (pointer) {
+        for (var i = 0; i < questions[pointer].answer.length; i++) {
+            var test = questions[pointer].answer[i];
+            panel.append("<button type='button' class='btn btn-secondary'>" + test + "</button><br>");
+            
+        }
+
+    },
+    timer: function () {
+
+    },
 }
-
-//  The decrement function.
-function decrement() {
-
-  //  Decrease number by one.
-  number--;
-
-  //  Show the number in the #show-number tag.
-  $("#show-number").html("<h2>" + number + "</h2>");
-
-
-  //  Once number hits zero...
-  if (number === 0) {
-
-    //  ...run the stop function.
-    stop();
-
-    //  Alert the user that time is up.
-    alert("Time Up!");
-  }
-}
-
-//  The stop function
-function stop() {
-
-  //  Clears our intervalId
-  //  We just pass the name of the interval
-  //  to the clearInterval function.
-  clearInterval(intervalId);
-}
-
-//  Execute the run function.
-run();
